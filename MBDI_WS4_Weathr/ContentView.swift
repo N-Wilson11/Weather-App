27de9237066298cf8026b761d5f19595b3356f2f
Nathan Wilson <nathankishawn@gmail.com>
2025-02-14 10:24:26 +0100

    Initial Commit

---
+++ MBDI_WS4_Weathr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MBDI_WS4_Weathr
+//
+//  Created by Nathan Wilson on 14/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Weather App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

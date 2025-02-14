e2be1e6f7d7a73b75b117c5acee5bec461aeb2db
Nathan Wilson <nathankishawn@gmail.com>
2025-02-14 10:26:00 +0100

    Initial commit

--- MBDI_WS4_Weathr/ContentView.swift
+++ MBDI_WS4_Weathr/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Weather App!")
+            Text("Weather App")
         }
         .padding()
     }
